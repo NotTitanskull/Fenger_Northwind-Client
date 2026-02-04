61e22149d031642f7b3246a0fd674bde1b2a5664
Christian Fenger <fengercs@gmail.com>
2026-02-03 22:21:54 -0600

    Implemented Bootstrap Grids & Toasts

---
+++ js/discount.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const discountRowEl = document.getElementById("discount-row");
+  const toastEl = document.getElementById("liveToast");
+
+  const toastProductEl = document.getElementById("toastProduct");
+  const toastTitleEl = document.getElementById("toastTitle");
+  const toastCodeEl = document.getElementById("toastCode");
+
+  const toast = bootstrap.Toast.getOrCreateInstance(toastEl, {
+    autohide: false,
+  });
+
+  discountRowEl.addEventListener("click", function (event) {
+    if (event.target.classList.contains("discount")) {
+      event.preventDefault();
+
+      const link = event.target;
+
+      toastProductEl.textContent = link.dataset.product;
+      toastTitleEl.textContent = link.dataset.discountTitle;
+      toastCodeEl.textContent = link.dataset.discountCode;
+
+      toast.show();
+    }
+  });
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      toast.hide();
+    }
+  });
+});
